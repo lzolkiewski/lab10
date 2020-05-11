7cbb4c189d83aec3789fb494c96474d0a4de24a7
Lucas <lukaszolkiewski@gmail.com>
2020-05-11 11:08:31 +0200

    initial commit

---
+++ src/main/java/wizut/tpsi/ogloszenia/DemoApplication.java
@@ -0,0 +1,13 @@
+package wizut.tpsi.ogloszenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
